8f22f30f7125b5ef93a65c8fc99180fc42d3e1c5
Powarut <jesnosmark@hotmail.com>
2023-11-07 01:42:15 +0700

    edit

--- server.js
+++ server.js
@@ -439,9 +439,106 @@ app.post('/foodtest',cors(), (req, res) => {
 // end food API----------------------------------------------------------------
 
 //API add cart 
+app.post("/addMenutoCart", (req, res) => {
+  const {
+    mem_id,
+    food_id,
+    quantity
+  } = req.body
+  console.log(req.body)
+  const sql = "INSERT INTO cart(mem_id, food_id, quantity) VALUES(?, ?, ?)"
+  conn.execute( 
+    sql,
+    [
+      mem_id,
+      food_id,
+      quantity
+    ],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({
+          message: err.message,
+        });
+        return;
+      }
+      res.status(201).json({
+        message: "เพิ่มเมนูลงตะกร้าแล้ว",
+        data: {
+          mem_id,
+          food_id,
+          quantity
+        }
+      });
+    }
+  )
+})
+//end
 
-//end   
+// get menu in cart
+app.get("/menuInCart", (req, res) => {
+  const {
+    mem_id,
+    status
+  } = req.body
+  const sql = "SELECT * FROM cart WHERE mem_id = ? AND status = ?"
+  conn.execute(
+    sql,
+    [
+      mem_id,
+      status
+    ],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({
+          message: err.message,
+        });
+        return;
+      }
+      res.status(201).json({
+        message: "เมนูในตะกร้า",
+        data: result,
+      });
+    }
+  )
+})//END
 
+// API edit quantity in cart
+app.put("/editcart", (req, res) => {
+    const {
+      quantity,
+      mem_id,
+      food_id,
+      cart_id,
+      status
+    } =  req.body
+ 
+    if (status === 0) {
+      const sql = "UPDATE cart SET quantity = ? WHERE mem_id = ? AND food_id = ? AND cart_id AND status = ?"
+      conn.execute(
+        sql,
+        [
+          quantity,
+          mem_id,
+          food_id,
+          cart_id,
+          status
+        ],
+        (err, result) => {
+          if (err) {
+            res.status(500).json({
+              message: err.message,
+            });
+            return;
+          }
+          res.status(201).json({
+            message: "เปลี่ยนแปลงจำนวนแล้ว",
+            data: result,
+          });
+        }
+      )
+    }
+})
+//END
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
