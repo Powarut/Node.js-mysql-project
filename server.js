43ecb191ae6b9ab286ef9cce53f4e728d4958e67
Pradit Yamsook <praditcry@gmail.com>
2023-11-22 23:01:31 +0700

    [fix]: call database connection from env

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use(express.urlencoded({extended: true}))
 
 
 const conn = mysql2.createConnection({
-  port : '3306',
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database:'project'
+  port : process.env.DB_PORT,
+  host: process.env.DB_URL,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 //API read all Members
